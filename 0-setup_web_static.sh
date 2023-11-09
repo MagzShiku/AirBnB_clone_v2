8ff8d5dc020a14fe41c03339be107b969a1d9757
JenMora <jeniffermourah73@gmail.com>
2023-11-09 02:02:27 -0800

    a Bash script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,7 +10,13 @@ fi
 sudo mkdir -p /data/web_static/{releases/test,shared}
 
 # Create a fake HTML file
-echo "<html><head></head><body>Test Page</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 
 # Create symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
