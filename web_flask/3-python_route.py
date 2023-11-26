b1eb512f7ea81855734cadf942c40ae8b5d83b5e
JenMora <jeniffermourah73@gmail.com>
2023-11-26 02:29:44 -0800

    This is a script that starts a Flask web application

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -36,7 +36,7 @@ def c_text(text):
 
 # Route: /python/<text>
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text='is cool'):
+def python_text(text):
     # Replace underscores with spaces
     processed_text = text.replace('_', ' ')
     return 'Python {}'.format(processed_text)
