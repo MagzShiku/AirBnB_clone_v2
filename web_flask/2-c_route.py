a257163b3e64ec3ee973b80549501d5266641a08
Ubuntu <ubuntu@ip-172-31-19-71.eu-north-1.compute.internal>
2023-11-28 13:19:56 +0000

    2-c_route.py - rev_01

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application:"""
+"""import flask"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """string 1"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """string 2"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ display “C ” followed by the value of the text variable"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    """main function"""
+    app.run(host='0.0.0.0', port=5000)
