b12e32702dafdab8be070b2904839cd25353d8cf
Ubuntu <ubuntu@ip-172-31-19-71.eu-north-1.compute.internal>
2023-11-28 12:43:36 +0000

    0-hello_route.py - rev_01

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """return the string ('Hello HBNB!)"""
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     """main caller"""
