a702430869c5fbc8defa31332cb8bf9307a83591
Ubuntu <ubuntu@ip-172-31-19-71.eu-north-1.compute.internal>
2023-11-28 12:50:26 +0000

    1-hbnb_route.py - rev_01

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application:"""
+
+
+from flask import Flask
+"""import Flask from falsk"""
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """function that returns a string"""
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    """main function"""
+    """run at the 0.0.0.0, & 5000 port"""
+    app.run(host='0.0.0.0', port=5000)
